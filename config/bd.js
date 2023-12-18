2c93a349cacffe12e38cfe3b8bcd658122fa63d4
ArturSponov <129497233+ArturSponov@users.noreply.github.com>
2023-12-18 15:48:35 +0600

    Update bd.js

--- config/bd.js
+++ config/bd.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoUrl: 'mongodb+srv://kamartur778:ganFijA6DLGEKUWS@cluster1.cxubszj.mongodb.net/?retryWrites=true&w=majority'
+    mongoUrl: 'mongodb+srv://kamartur778:ganFijA6DLGEKUWS@cluster1.cxubszj.mongodb.net/test?retryWrites=true&w=majority'
     
     
     // mongodb+srv://kamartur778:oPa0j6q0YXecSdPS@cluster0.pxcvidg.mongodb.net/?retryWrites=true&w=majority",
